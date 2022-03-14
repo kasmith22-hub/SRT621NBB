1af162711f4c9a69715cb518df5a82044b30eb26
kiki <kikibu2sweet@yahoo.com>
2022-03-14 18:02:53 -0400

    adding code

--- Web_Server/web.js
+++ Web_Server/web.js
@@ -16,4 +16,20 @@ const app = http.createServer((request, response) => {
 app.listen(port);
 console.log('The Server has started and is listening on port number: ${port}');
 
+var NowDate = new Date();
+var Full_Data = NowDate.getFullYear()+'-'+(NowDate.getMonth()+1)+'-'+NowDate.getDate();
+var Fullhour = NowDate.getHours();
+var hr = NowDate.getHours();
+var min = NowDate.getMinutes();
+var sec = NowDate.getSeconds();
+var amOrpm = 'AM';
+
+if(hr == 0){
+    hr = 12;
+}
+if(hr > 12){
+    hr = hr - 12;
+    amOrpm = "PM";
+}     
+console.log ('${hr}:${min} :${sec} ${amOrpm} ${Full_Data}');
 
