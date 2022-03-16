c695aa7302db65d58b6727a84338d0ba882f7e55
kiki <kikibu2sweet@yahoo.com>
2022-03-15 21:19:37 -0400

    changes to web

---
+++ Web/public/js/index.js
@@ -0,0 +1,20 @@
+var NowDate = new Date();
+var Full_Data = NowDate.getFullYear()+'-'+(NowDate.getMonth()+1)+'-'+NowDate.getDate();
+var Fullhour = NowDate.getHours();
+var hr = NowDate.getHours();
+var min = NowDate.getMinutes();
+var sec = NowDate.getSeconds();
+var amOrpm = 'AM';
+
+if(hr == 0){
+    hr = 12;
+}
+if(hr > 12){
+    hr = hr - 12;
+    amOrpm = "PM";
+}     
+// var k= document.write =  + hr+':'+min+':'+sec +' '+amOrpm + '<br>' +Full_Data;
+// document.getElementById('date').innerHTML = k;
+
+console.log( 'The time is :'+ hr +':'+min+':'+sec +' '+amOrpm + ' Date is ' + Full_Data);
+document.getElementById('date').innerHTML = 'The time is :'+ hr +':'+min+':'+sec +' '+amOrpm + ' Date is ' + Full_Data;
--- Web/web.js
+++ Web/web.js
@@ -1,38 +1,26 @@
-const port = 8989;
- const http = require("http");                                          
- const httpStatus = require("http-status-codes");
-  const app = http.createServer((request, response) => {                  
-    console.log("Received an incoming request!");
-    response.writeHead(httpStatus.OK, {
+const port = 8989,
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  fs = require("fs");                                     
+const routeMap = {                                        
+  "/": "view/index.html",
+  };
+
+http
+  .createServer((req, res) => {
+    res.writeHead(httpStatus.OK, {
       "Content-Type": "text/html"
-    });                                                             
+    });
+    if (routeMap[req.url]) {
+      fs.readFile(routeMap[req.url], (error, data) => {   
+        res.write(data);                                 
+        res.end();
+      });
+    } else {
+      res.end("<h1>Sorry, not found.</h1>");
+    }
+  })
+  .listen(port);
+console.log(`The server has started and is listening
+ on port number: ${port}`);
 
-    let responseMessage = "<h1>Welcome SRT621</h1>";
-    response.write(responseMessage);
-    response.end();
-    console.log(`Sent a response : ${responseMessage}`);
-  });
-
-  var NowDate = new Date();
-var Full_Data = NowDate.getFullYear()+'-'+(NowDate.getMonth()+1)+'-'+NowDate.getDate();
-var Fullhour = NowDate.getHours();
-var hr = NowDate.getHours();
-var min = NowDate.getMinutes();
-var sec = NowDate.getSeconds();
-var amOrpm = 'AM';
-
-if(hr == 0){
-    hr = 12;
-}
-if(hr > 12){
-    hr = hr - 12;
-    amOrpm = "PM";
-}     
-// document.write =  + hr+':'+min+':'+sec +' '+amOrpm + '<br>' +Full_Data;
-
-console.log( 'The time is :'+ hr +':'+min+':'+sec +' '+amOrpm + ' Date is ' + Full_Data);
-
-
-app.listen(port);                                                   
-console.log(`The server has started and is listening on port number:
-${port}`);
